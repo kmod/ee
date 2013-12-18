4a2d68813e17cc257b1d7adade2ba950c4ae9443
Kevin Modzelewski <kevmod@gmail.com>
2013-12-18 14:20:40 -0800

    I think counter is right but the electronics are broken

--- modboard/programming/cpld.v
+++ modboard/programming/cpld.v
@@ -20,16 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 module cpld(
     input p3B2,
+    input pG0,
     //input b,
-    output reg p3A0,
-    output reg p3A1,
-    output reg p3A2,
-    output reg p3A3
+    output p3A0,
+    output p3A1,
+    output p3A2,
+    output p3A3
 
     );
 
-    always @(posedge p3B2) begin
-        {p3A3, p3A2, p3A1, p3A0} <= {p3A3, p3A2, p3A1, p3A0} + 1;
+    reg [13:0] ctr = 0;
+    assign {p3A3, p3A2, p3A1, p3A0} = ctr[13:10];
+    always @(posedge pG0) begin
+        ctr <= ctr + 1;
     end
     //assign p3A3 = p3B2;
     //assign p3A2 = !p3B2;
