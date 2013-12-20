533d78909b0afac04ff3bdf3b3401c2cd2e4c11e
Kevin Modzelewski <kevmod@gmail.com>
2013-12-20 02:31:52 -0800

    kind of got the basic system working, though it has a lot of robustness issues

--- modboard/programming/cpld.v
+++ modboard/programming/cpld.v
@@ -19,22 +19,29 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module cpld(
+    input p3B1,
     input p3B2,
     input pG0,
-    //input b,
+    input pRST,
+    output p2A2,
+    output p2A3,
     output p3A0,
     output p3A1,
     output p3A2,
     output p3A3
-
     );
 
-    reg [13:0] ctr = 0;
-    assign {p3A3, p3A2, p3A1, p3A0} = ctr[13:10];
-    always @(posedge pG0) begin
-        ctr <= ctr + 1;
-    end
-    //assign p3A3 = p3B2;
-    //assign p3A2 = !p3B2;
+    assign p2A2 = 1;
+    assign p2A3 = !p3B1;
+
+    assign p3A1 = pRST;
+
+    //reg [13:0] ctr = 0;
+    //assign {p3A3, p3A2, p3A1, p3A0} = ctr[13:10];
+    //always @(posedge pG0) begin
+        //ctr <= ctr + 1;
+    //end
+    assign p3A3 = p3B2;
+    assign p3A2 = !p3B2;
 
 endmodule
