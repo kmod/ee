1f1bd26f79b3901b4a50d2eb3cf1a1ce45c1ac71
Kevin Modzelewski <kevmod@gmail.com>
2013-12-26 22:54:15 -0800

    move things around more

--- modboard/programming/gen.py
+++ modboard/programming/gen.py
@@ -1,26 +1,34 @@
+import cStringIO
 import collections
 import re
 import sys
 
+from model import AssemblyPin
 import parser
 
-class Output(object):
+class RoutingNetwork(object):
     def __init__(self, assem):
         self.a = assem
 
-        self.routers = {}
-        for boardname in assem.boards:
-            boarddef = assem.boards[boardname][0]
-            for r in boarddef.routers:
-                self.routers["%s.%s" % (boardname, r)] = {}
+        self.assignments = {}
+        # self.routers = {}
+        # for boardname in assem.boards:
+            # boarddef = assem.boards[boardname][0]
+            # for r in boarddef.routers:
+                # self.routers["%s.%s" % (boardname, r)] = {}
 
-    def available(self, (boardname, socket, pinname)):
-        r = self.a.getRouter((boardname, socket, pinname))
+    def available(self, pin):
+        return pin not in self.assignments
+        r = self.a.getRouter(pin)
         if not r:
             return True
         print r
         1/0
         return 1
+
+    def addPath(self, path):
+        print path
+        1/0
 
 class Router(object):
     def __init__(self, output, assem):
@@ -31,16 +39,16 @@ class Router(object):
         board, pin = pin.split('.', 1)
         boarddef = self.a.boards[board][0]
         pin = boarddef.pins[pin]
-        return (board, pin[0], pin[1])
+        return AssemblyPin(board, pin[0], pin[1])
 
     def _flipPin(self, pin):
-        board, socket, pin = pin
-        boarddef = self.a.boards[board][0]
-        conn = self.a.connections[board].get(socket)
+        assert isinstance(pin, AssemblyPin)
+        # boarddef = self.a.boards[pin.boardname][0]
+        conn = self.a.connections[pin.boardname].get(pin.socket)
         if not conn:
             return None
         nextboard, nextboard_socket = conn
-        return nextboard, nextboard_socket, pin
+        return AssemblyPin(nextboard, nextboard_socket, pin.pinname)
 
     def route(self, target, source):
         target = self._mapPin(target)
@@ -53,6 +61,7 @@ class Router(object):
         def add(curpath, next):
             if next is None:
                 return
+            assert isinstance(next, AssemblyPin)
             if not self.o.available(next):
                 return
             if next not in seen:
@@ -71,32 +80,32 @@ class Router(object):
             flipped = self._flipPin(last)
             add(curpath, flipped)
 
-            if last[2] not in ('g0', 'grst'):
-                assert re.match("[a-e][0-3]", last[2]), last
+            if last.pinname not in ('g0', 'grst'):
+                assert re.match("[a-e][0-3]", last.pinname), last
 
                 # print pin_attrs
                 # if 'port' in pin_attrs:
                 router = self.a.getRouter(last)
                 if router:
                     for dest_socket, dest_pin in router[2].values():
-                        add(curpath, (last[0], dest_socket, dest_pin))
+                        add(curpath, AssemblyPin(last.boardname, dest_socket, dest_pin))
 
         raise Exception("Could not route %s to %s!" % (source, target))
 
 def process(assem):
-    o = Output(assem)
+    rn = RoutingNetwork(assem)
 
-    r = Router(o, assem)
+    r = Router(rn, assem)
     for a in assem.assignments:
         target, source = a
         path = r.route(target, source)
-        print path
-    return o
+        rn.addPath(path)
+    return rn
 
 def main():
     in_fn, out_fn = sys.argv[1:]
 
-    board_defs, assemblies = parser.parse(in_fn)
+    board_defs, assemblies, included_files = parser.parse(in_fn)
 
     for board_name, b in board_defs.items():
         for sid, sdefs in b.sockets.values():
@@ -104,15 +113,18 @@ def main():
                 for n in "0123":
                     assert (c+n) in sdefs, (board_name, sid)
 
+    of = cStringIO.StringIO()
+    print >>of, "%s: %s" % (out_fn, ''.join(included_files))
+
     for assem in assemblies:
         print
         print "Processing assembly", assem.name
-        process(assem)
+        rn = process(assem)
+        doOutput(rn, of)
 
-    # output(modules, out_fn)
+    output = of.str()
 
-    # of = open(out_fn, 'w')
-    # print >>of, "%s: %s" % (out_fn, ''.join(p.included_files))
-    # of.close()
+    with open(out_fn, 'w') as of:
+        of.write(output)
 main()
 
--- modboard/programming/parser.py
+++ modboard/programming/parser.py
@@ -1,4 +1,7 @@
+import functools
 import os
+
+from model import Assembly, BoardDef
 
 class Parser(object):
     def __init__(self):
@@ -54,157 +57,41 @@ class Scope(object):
         assert name in self.handlers, (self, name, self.handlers.keys())
         return self.handlers[name]
 
-class BoardDef(object):
-    def __init__(self, name):
-        self.name = name
-        self.sockets = {}
-        self.routers = {}
-        self.pins = {}
-        self.jtag_entry = None
+class GlobalScope(Scope):
+    def __init__(self):
+        self.assemblies = []
+        self.board_defs = {}
 
-        self.sockets[None] = (-1, {})
+        super(GlobalScope, self).__init__(
+                board=self.boardHandler,
+                assembly=self.assemblyHandler,
+                )
 
-    def _setPin(self, name, pin):
-        assert name not in self.pins, (self.name, name)
-        self.pins[name] = pin
-
-    def addJtagEntry(self, args, opts):
-        assert not args
-        jtag = int(opts.pop('jtag'))
-        assert jtag > 0
-
-        assert not self.jtag_entry
-        self.jtag_entry = (jtag,)
-
-    def addSocket(self, args, opts):
-        jtag = int(opts.pop('jtag'))
-        assert jtag > 0
-        assert not opts, opts
-
-        name, = args
-        self.sockets[name] = (jtag, {})
-
-    def addRouter(self, args, opts):
-        jtag = int(opts.pop('jtag'))
-        assert jtag > 0
-        part = opts.pop('part')
-        assert not opts, opts
-
-        name, = args
-        self.routers[name] = (jtag, part, {})
-
-    def addPin(self, args, opts):
-        fullname, = args
-        socket = None
-
-        name = fullname
-        if '.' in fullname:
-            socket, name = fullname.split('.')
-
-        p = (socket, name, opts)
-        self._setPin(fullname, p)
-        if "alias" in opts:
-            self._setPin(opts['alias'], p)
-
-        if "port" in opts:
-            router_name, router_pin = opts['port'].split('.')
-            router = self.routers[router_name]
-            assert router_pin not in router[2]
-            router[2][router_pin] = (socket, name)
-
-        assert socket in self.sockets
-        pins = self.sockets[socket][1]
-        assert name not in pins
-        pins[name] = p
-
-# TODO not as a global
-board_defs = {}
-def boardHandler(args, opts):
-    assert not opts
-    name, = args
-    assert name not in board_defs, name
-    b = BoardDef(name)
-    board_defs[name] = b
-    return Scope(
-            socket=b.addSocket,
-            router=b.addRouter,
-            pin=b.addPin,
-            jtag_entry=b.addJtagEntry,
-            )
-
-class Assembly(object):
-    def __init__(self, name):
-        self.name = name
-
-        self.boards = {}
-        self.assignments = []
-        self.connections = {}
-
-    def addBoard(self, args, opts):
+    def assemblyHandler(self, args, opts):
         assert not opts
-        boardname, id, connection = args
-        assert id not in self.boards
-        b = board_defs[boardname]
+        name, = args
+        a = Assembly(name)
+        self.assemblies.append(a)
+        return Scope(
+                board=functools.partial(a.addBoard, self.board_defs),
+                assign=a.addAssignment,
+                )
 
-        d = self.connections.setdefault(id, {})
-        if connection != "unconnected":
-            if '.' in connection:
-                conn_id, conn_socket = connection.split('.')
-            else:
-                conn_id, conn_socket = connection, None
-            assert conn_id in self.boards
-            conn_b = self.boards[conn_id][0]
-            assert conn_socket in conn_b.sockets
-            assert conn_socket not in self.connections[conn_id], (boardname, conn_id, conn_socket)
-
-            d[None] = (conn_id, conn_socket)
-            self.connections[conn_id][conn_socket] = (id, None)
-        self.boards[id] = (b,)
-
-    def addAssignment(self, args, opts):
-        assert opts.pop('') == ''
+    def boardHandler(self, args, opts):
         assert not opts
-        val = ' '.join(args[1:])
-
-        target = args[0]
-        board_id, name = target.split('.', 1)
-        assert board_id in self.boards
-
-        assert not [1 for (t, s) in self.assignments if t == target]
-        self.assignments.append((target, val))
-
-    # def getPinAttrs(self, (boardname, socket, pinname)):
-        # boarddef = self.boards[boardname][0]
-        # pin_attrs = boarddef.sockets[socket][1][pinname][2]
-        # return pin_attrs
-
-    def getRouter(self, (boardname, socket, pinname)):
-        boarddef = self.boards[boardname][0]
-        pin_attrs = boarddef.sockets[socket][1][pinname][2]
-
-        if 'port' not in pin_attrs:
-            return None
-
-        router_name, router_pin = pin_attrs['port'].split('.')
-        router = boarddef.routers[router_name]
-        return router
-
-assemblies = []
-def assemblyHandler(args, opts):
-    assert not opts
-    name, = args
-    a = Assembly(name)
-    assemblies.append(a)
-    return Scope(
-            board=a.addBoard,
-            assign=a.addAssignment,
-            )
+        name, = args
+        assert name not in self.board_defs, name
+        b = BoardDef(name)
+        self.board_defs[name] = b
+        return Scope(
+                socket=b.addSocket,
+                router=b.addRouter,
+                pin=b.addPin,
+                jtag_entry=b.addJtagEntry,
+                )
 
 def parse(fn):
-    global_scope = Scope(
-            board=boardHandler,
-            assembly=assemblyHandler,
-            )
+    global_scope = GlobalScope()
     p = Parser()
     p.parse(fn, global_scope)
-    return board_defs, assemblies
+    return global_scope.board_defs, global_scope.assemblies, p.included_files
