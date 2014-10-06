ea1d2a891dc85ea42b322ac3b705acb97abd7ea6
Kevin Modzelewski <kevmod@gmail.com>
2014-10-06 04:23:29 -0700

    urgh some sort of timing issue here

--- stm32_discovery/f3_blink/blink.c
+++ stm32_discovery/f3_blink/blink.c
@@ -10,7 +10,25 @@ void delay(int count)
 {
     for (int i = 0; i < count; i++)
     {
-        __asm__ volatile ("nop");
+        __asm__ volatile ("");
+    }
+}
+
+// helper function for debugging in gdb:
+GPIO_TypeDef* gpio(int i) {
+    switch (i) {
+        case 0:
+            return GPIOA;
+        case 1:
+            return GPIOB;
+        case 2:
+            return GPIOC;
+        case 3:
+            return GPIOD;
+        case 4:
+            return GPIOE;
+        default:
+            return 0;
     }
 }
 
@@ -22,27 +40,34 @@ int main(void)
         | RCC_AHBENR_GPIOEEN
         ;
 
+    // I don't know why this is required, but sometimes we need to delay
+    // between enabling the GPIO clock and setting the registers.
+    delay(5000);
 
     GPIOE->MODER = 0x55555555;
-    //GPIOE->OTYPER = 0x0;
-    //GPIOE->OSPEEDR = 0x0;
-    //GPIOE->PUPDR = 0x0;
+    GPIOE->OTYPER = 0x0;
+    GPIOE->OSPEEDR = 0x0;
+    GPIOE->PUPDR = 0x0;
     //GPIOE->MODER = (GPIOE->MODER & ~GPIO_MODER_MODER10) | (GPIO_MODER_MODER10_0); // OUTPUT
     //GPIOE->OTYPER = (GPIOE->OTYPER & ~GPIO_OTYPER_OT_10) | (0); // push-pull
     //GPIOE->OSPEEDR = (GPIOE->OSPEEDR & ~GPIO_OSPEEDER_OSPEEDR10) | (0); // low speed
     //GPIOE->PUPDR = (GPIOE->PUPDR & ~GPIO_PUPDR_PUPDR10) | (0); // low speed
 
+    for (int i = 0; i < 3; i++) {
+        delay(800000);
+        GPIOE->ODR = ~0;
+        delay(800000);
+        GPIOE->ODR = 0;
+    }
+
     for (;;)
     {
-        // Around 1/4 of a second
-        delay(1000000);
+        delay(15000000);
 
-        // Turn all pins on
         GPIOE->ODR = ~0;
 
         delay(1000000);
 
-        // Turn all pins off
         GPIOE->ODR = 0;
     }
 }
